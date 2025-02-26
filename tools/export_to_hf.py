a47d3d4c48499498de3de9d55529d7232a96e240
Davis Wertheimer <dww78@cornell.edu>
2025-02-26 10:59:02 -0500

    Conversion

--- tools/export_to_hf.py
+++ tools/export_to_hf.py
@@ -1,8 +1,8 @@
 from dolomite_engine.hf_models import export_to_huggingface
 
 
-load_path = "/gpfs/davis/results/granite_tune/30b-4x/hf-convert/"
-save_path = "/gpfs/davis/results/granite_tune/30b-4x/hf-dolo-convert/"
+load_path = "/gpfs/davis/results/granite_tune/7b-4x/hf-convert/"
+save_path = "/gpfs/davis/results/granite_tune/7b-4x/hf-dolo-convert/"
 
 # export to HF llama
 export_to_huggingface(load_path, save_path, model_type="granitemoeshared")
